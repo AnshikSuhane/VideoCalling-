28d80f3802411fef6dd2bf556dfc8b9f6398281f
AnshikSuhane <anshiksuhane9@gmail.com>
2025-12-31 00:18:35 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,16 @@
-import express from  "express"
-import "dotenv"
+import express from "express";
+import { ENV } from "./lib/env.js";
+import cors from "cors";
 
-const app=express()
+
+const app = express();
+app.use(cors());
+
+app.get("/", (req, res) => {
+  console.log("This is my first Testing API");
+  res.status(200).json({success:true,message:"API is working"});
+});
+
+app.listen(ENV.PORT, () => {
+  console.log("Server is running on PORT:", PORT);
+});
